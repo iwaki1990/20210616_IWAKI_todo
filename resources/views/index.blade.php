a84a5a8bf15095fbd397f34a98503df600024e5c
iwaki1990 <low.son.wing@gmail.com>
2021-06-23 16:22:43 +0900

    16 commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,10 +9,9 @@
   <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
 </head>
 <body>
-  @if (count($errors) > 0)
+@if (count($errors) > 0)
     <p>入力に問題があります</p>
   @endif
-  @foreach($items as $item)
 <th $guarded = array('id')>
   <div class="container">
     <div class="card">
@@ -23,27 +22,38 @@
             <input class="button-add" type="submit" value="追加" />
           </form>
           @csrf
+          <tr>
+            <th>作成日</th>
+            <th>タスク名</th>
+            <th>更新</th>
+            <th>削除</th>
+          </tr>
+          <tr>
+          @foreach($items as $item)
+          @csrf
             <td>
             {{ \Carbon\Carbon::now()->format("Y年m月d日 H:i:s") }}
               {{$item->created_at}}
+            </td>
             </td>
             <form action="/todo/update/{{$item->id}}" method="post">
               @csrf
               <td>
                 <input type="text" class="input-update" value="{{$item->content}}" name="content" />
               </td>
+              @endforeach
               <td>
+              <form action="/todo/update" method="post">
                 <input class="button-update" type="submit" value="更新">
               </td>
             </form>
             <td>
               <form action="/todo/delete" method="post">
                 <input type="hidden" name="_token" value="削除">
-                <button class="button-delete">削除</button>
               </form>
             </td>
           </tr>
-          @endforeach
+          </table>
       </div>
     </div>
   </div>
